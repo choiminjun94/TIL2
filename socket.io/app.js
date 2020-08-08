40eb04c472d31990a730e49ca07a4a452025f484
choiminjun <1020alswns@gmail.com>
2020-08-08 14:26:36 +0900

    Schema 작성

---
+++ socket.io/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const flash = require('connect-flash');
+require('dotenv').config();
+
+const indexRouter = require('./routes');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.set('port', process.env.PORT || 8005);
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use()
