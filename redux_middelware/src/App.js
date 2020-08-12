86199975a8bf12efab5a84de923f786ec828e7ed
choiminjun <1020alswns@gmail.com>
2020-08-12 18:17:57 +0900

    0812: 리펙토리&& Saga내부에서 현재상태를 조회하는 방법

--- redux_middelware/src/App.js
+++ redux_middelware/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import SampleContainer from './container/SampleContainer';
+import CounterContainer from './container/CounterContainer';
 
 const App = () => {
   return (
     <div>
-      <SampleContainer />
+      <CounterContainer />
     </div>
   );
 };
