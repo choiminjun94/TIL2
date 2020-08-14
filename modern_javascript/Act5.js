d33c40555cb369d34b25b2e141a2211341568db7
choiminjun <1020alswns@gmail.com>
2020-08-14 11:39:09 +0900

    0814|4장 표현식과 연산자

---
+++ modern_javascript/Act5.js
@@ -0,0 +1,13 @@
+//우선 연산자 순위
+let test = 2+(2*4);
+console.log(test);
+// 우선 연산자 순위2
+let test2 = (24/6) * 4+2;
+console.log(test2);
+
+let stringTest = "시"+"발"
+console.log(stringTest);
+
+let a = 3
+console.log("a값"+a+"테스트");
+
