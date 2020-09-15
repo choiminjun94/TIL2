621e3f19fbdbe7ef5ab2beaaf8c7eb5677df1072
choiminjun <1020alswns@gmail.com>
2020-09-15 15:50:03 +0900

    0915|multer

--- boilerplate-mern-stack-master/client/src/components/utils/FileUPload.js
+++ boilerplate-mern-stack-master/client/src/components/utils/FileUPload.js
@@ -1,49 +1,64 @@
-import React from "react";
-import Dropzone from "react-dropzone";
+import React, {useState} from "react";
+import Dropzone from 'react-dropzone'
 import { Icon } from 'antd'
 import axios from 'axios'
 
 function FileUpload() {
 
-    const dropHandler =(files)=>{
-        let formDate = new formDate();
-        const config ={
-            header: {'content-type': 'multipart/form-data'}
-            //header 어떤 파일인지 컨텐트  타입을 정의해서 백에서 파일을 받을때 에러가 없이 받을수 있게 해준다
-        }
-        formDate.append("file", files[0])
-        //formDate 파일의 정보 가 append를 이용해 더해서 들어간다.
-        axios.post('/api/product/image',formDate ,config)
-        .then(response =>{
-            if(response.data.success){
+const [Images, setImages] = useState([])
+//useState([]) 몇개의 파일이 올라 갈지 모르기 때문에
 
-            }else{
-                alert('실패')
-            }
-        })
+
+const dropHandler =(files)=>{
+    let formData = new FormData();
+    // 올리는 파일의 정보
+
+    const config ={
+        header: {'content-type': 'multipart/form-data'}
+        //파일의 컨텐츠 타입을 알려주어야 한다.
     }
+
+    formData.append("file", files[0])
+
+    axios.post('/api/product/image',formData ,config)
+    // 파일 전송시 formData, config를 넣어 주어여 한다.
+
+    .then(response => {
+        if(response.data.success){
+            console.log(response.data);
+            setImages([...Images, response.data.filePath ])
+        }else{
+            alert("파일 보내기 실패")
+        }
+    })
+}
+
   return (
     <div style={{display: 'flex', justifyContent: 'space-between'}}>  
-      <Dropzone onDrop={(acceptedFiles) => console.log(acceptedFiles)}>
+      <Dropzone onDrop={dropHandler}>
         {({ getRootProps, getInputProps }) => (
           <section>
-            <div
-              style={{
-                width: 300,
-                height: 240,
-                border: "1px solid lightgray",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-              {...getRootProps()}
-            >
+            <div style={{ 
+                width: 300, height: 240, border: '1px solid lightgray',
+                display: 'flex', alignItems: 'center', justifyContent: 'center'
+            }}
+            {...getRootProps()}>
               <input {...getInputProps()} />
-              <Icon type="plus" style={{ fontSize: "3rem" }} />
+              <Icon type="plus" style={{fontSize:'3rem'}} />
             </div>
           </section>
         )}
       </Dropzone>
+      {/* 파일을 올리면 사진이 나올수 있게 조치 */}
+      <div style={{display: 'flex', width: '350px', height: '240px', overflowX: 'scroll'}}> 
+            {Images.map((image, index) =>(
+              <div key={index}>
+                <img style={{minWidth: '300px', width: '300px', height: '240px'}}
+                  src={`http://localhost:5000/${image}`}
+                />
+              </div>
+            ))}
+      </div>
     </div>
   );
 }
--- boilerplate-mern-stack-master/server/routes/product.js
+++ boilerplate-mern-stack-master/server/routes/product.js
@@ -1,13 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer')
 
 
 //=================================
 //             Product
 //=================================
-app.post('/image', (req, res) =>{
-    //기져온 이미지를 저장해준다.
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+      cb(null, `${Date.now()}_${file.originalname}`)
+      // 먼저 현재 날짜 | file이름
+    }
+  })
+   
+  var upload = multer({ storage: storage }).single("file")
+
+  router.post('/image', (req,res) => {
+    //가져온 이미지를 저장해 준다. 그러기 위해서 multer라른 라이브러리가 필요
+    upload(req,res, err =>{
+        if(err){
+            return req.json({success: false, err})
+        } 
+        return res.json({success: true, filePath:res.req.file.path, filename: res.req.file.filename})
+    })
 })
+
+
 
 
 module.exports = router;
