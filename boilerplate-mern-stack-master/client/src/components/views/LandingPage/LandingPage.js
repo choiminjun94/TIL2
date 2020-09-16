869fa34f1d22970fc5e5e4c7f05e1e8e1841f95f
choiminjun <1020alswns@gmail.com>
2020-09-16 11:15:29 +0900

    0916|DB에서 가져온 정보를 카드에 넣어주기

--- boilerplate-mern-stack-master/client/src/components/views/LandingPage/LandingPage.js
+++ boilerplate-mern-stack-master/client/src/components/views/LandingPage/LandingPage.js
@@ -1,27 +1,58 @@
-import React,{useEffect} from 'react'
+import React, { useEffect, useState } from "react";
 import { FaCode } from "react-icons/fa";
-import axios from 'axios'
+import axios from "axios";
+import { Icon, Col, Card, Row } from "antd";
+import Meta from "antd/lib/card/Meta";
 
-function LandingPage() {    
-    useEffect(() => {
-        //body   
-        axios.post('/api/product/products')
-        .then(response =>{
-            if(response.data.success){
-                console.log(response.data);
-                
-            }else{
-                alert("상품 가져오는데 실패")
-            }
-        })
-    }, [])
+function LandingPage() {
+  const [Products, setProducts] = useState([]);
+
+  useEffect(() => {
+    //body
+    axios.post("/api/product/products").then((response) => {
+      if (response.data.success) {
+        console.log(response.data);
+        setProducts(response.data.productInfo);
+      } else {
+        alert("상품 가져오는데 실패");
+      }
+    });
+  }, []);
+
+  const renderCards = Products.map((product, index) => {
+    console.log("product", product);
 
     return (
-        <div>
-            렌딩 페이지
-        </div>
-    )
+      <Col lg={6} md={8} xs={25} key={index}>
+        <Card
+          cover={<img src={`http://localhost:5000/${product.images[0]}`} />}
+        >
+          <Meta title={product.title} description={`$${product.price}`} />
+        </Card>
+      </Col>
+    );
+  });
 
+  return (
+    <div style={{ width: "75%", margin: "3rem auto" }}>
+      <div style={{ textAlign: "center" }}>
+        <h2>
+          let travel anywhere
+          <Icon type="rocket" />
+        </h2>
+      </div>
+      {/* Filter */}
+      {/* Search */}
+      {/* Cards */}
+      <Row gutter={[16,16]}>
+          {/* gutter - 여백 */} 
+          {renderCards}
+      </Row>
+      <div style={{ justifyContent: "center" }}>
+        <button>더보기</button>
+      </div>
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
